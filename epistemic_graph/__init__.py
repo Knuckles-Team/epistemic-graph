4076ca1a740927a30953f73bf4e6d02300c8018c
knucklessg1 <knucklessg1@gmail.com>
2026-05-31 18:23:52 -0500

    Bump version: 0.17.2 → 0.17.3

--- epistemic_graph/__init__.py
+++ epistemic_graph/__init__.py
@@ -1,3 +1,4 @@
 from .client import EpistemicGraphClient, SyncEpistemicGraphClient
+from .parser import RustASTParser
 
-__all__ = ["EpistemicGraphClient", "SyncEpistemicGraphClient"]
+__all__ = ["EpistemicGraphClient", "SyncEpistemicGraphClient", "RustASTParser"]
